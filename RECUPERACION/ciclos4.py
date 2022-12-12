e3d098f92c9b2b4be5751d87554988cd85a93e77
stiven1618 <116227845+stiven1618@users.noreply.github.com>
2022-12-12 09:10:07 -0500

    Add files via upload

---
+++ RECUPERACION/ciclos4.py
@@ -0,0 +1,9 @@
+num = int(input("ingresa un número : "))
+cont = 0
+for i in range (2,num,1):
+    if num%i==0:
+        cont +=1
+if cont>0:
+    print ("el número no es primo")
+else:
+    print ("el número es primo")
---
+++ RECUPERACION/condicio5.py
@@ -0,0 +1,14 @@
+numero_1 = float(input("coloca la calificacion del estudiante: "))
+if(numero_1<=10 and numero_1>=9):
+    print("Bien")
+elif(numero_1<=8 and numero_1>=7):
+    print("suficiente")
+elif(numero_1<=6 and numero_1>=5):
+    print("insuficiente")
+elif(numero_1<=4 and numero_1>=3):
+    print("insuficiente")
+elif(numero_1<=2 and numero_1>=1):
+    print("insuficiente")
+else:
+    print("La nota es buena")
+
